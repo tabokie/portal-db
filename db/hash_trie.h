a05773990c546065146ffbd0bd7bcd52c2577b12
tabokie <james_199@126.com>
2019-03-21 15:22:47 +0800

    alpha version (single-thread) tested.

--- db/hash_trie.h
+++ db/hash_trie.h
@@ -14,13 +14,14 @@ namespace portal_db {
 
 namespace {
 
-// pointer		|	indication										||
-// 0					|	physically deleted, usable		 |
-// 0x0fff			|	linked-list tail							 |
-
-// value 			|	indication										||
-// 0x0fff			|	not initialized								 |
-// (0,prefix)	| logically deleted							 |
+// _pointer___|_indication__________________|
+//  0					|	physically deleted, usable	|
+//  0x0fff		|	linked-list tail						|
+//  n					|	(n-1) point to next					|
+// ------------------------------------------
+// _value_____|_indication__________________|
+//  0x0fff		|	not initialized							|
+// (0,prefix)	|	logically deleted						|
 struct HashNode {
 	std::atomic<int32_t> pointer;
 	uint32_t value;
@@ -50,7 +51,7 @@ struct HashTrieNode {
 	int32_t parent;
 	int32_t id;
 	unsigned char level;
-	// >=0 if pointed to local hash
+	// >0 if pointed to local hash (-1)
 	// <0 if pointed to global node
 	// ==0x0fff if null
 	std::atomic<int32_t> forward[256];
@@ -80,7 +81,7 @@ class HashTrie {
  				if(tmp >= nodes_.size())
  					return Status::Corruption("access exceeds `HashTrieNode` vector");
  				node = nodes_[tmp].get();
- 			} else if(tmp == 0) { // no prefix match
+ 			} else if(tmp == 0x0fffffff) { // no prefix match
  				break;
  			} else { // find match
 		    // quadratic proding
@@ -89,15 +90,17 @@ class HashTrie {
 		    int offset = 1;
 		    do{
 		    	HashNode& hnode = node->table[cur];
-		    	if(hnode.pointer != 0 && check_pull(hnode.value, key, ret)) return Status::OK();
+		    	// not deleted and pull
+		    	if(hnode.pointer != 0 && check_pull(hnode.value, key, ret)) 
+		    		return Status::OK();
 		      cur = (cur+ 2*offset - 1) % hash_size_;
 		      offset++;
 		    } while( offset < probe_depth_ && cur != hash_val);
-		    return Status::NotFound("missing key");
+		    return Status::NotFound("missing key match");
  			}
  			level ++;
  		}
- 		return Status::NotFound("missing key");
+ 		return Status::NotFound("missing level match");
  	}
  	Status Put(const Key& key, const Value& value) {
  		HashTrieNode<hash_size_>::UnsafeRef node = nodes_[0].get();
@@ -135,26 +138,31 @@ class HashTrie {
 			    do {
 		    		HashNode& hnode = node->table[cur];
 			    	int32_t tmp;
+			    	// node is deleted
 			    	if( (tmp=hnode.pointer) == 0) {
-			    		// std::cout << "found null at " << cur << std::endl;
+			    		// point to existing linked-list
 			    		if(std::atomic_compare_exchange_strong(&hnode.pointer, 
 			    			&tmp, 
 			    			node->forward[key[level]]
 			    			)) {
+			    			// allocate new memory
 			    			while(hnode.value == 0x0fffffff ) {
 			    				hnode.value = values_.New();
 			    			}
+			    			// write data to memory
 			    			char* p = values_.Get(hnode.value);
 			    			memcpy(p, key.raw_ptr(), 8);
 			    			value.write<0, 256>(p + 8);
-		    				tmp = hnode.pointer;
+		    				tmp = hnode.pointer; // assumed old head
+		    				// insert as linked-list head
 			    			while(!std::atomic_compare_exchange_strong(
 			    				&node->forward[key[level]], 
 			    				&tmp,
-			    				cur
+			    				cur + 1
 			    				)) { hnode.pointer = tmp; }
 		    				return Status::OK();
 			    		}
+			    		// or preempted
 			    		vacant --;
 			    	}
 			    	cur = (cur+ 2*offset - 1) % hash_size_;
@@ -170,43 +178,85 @@ class HashTrie {
 		    	));
 		    HashTrieNode<hash_size_>::UnsafeRef new_node = nodes_.back().get();
 		    int32_t old_idx = node->forward[key[level]];
-		    assert(old_idx >= 0);
+		    assert(old_idx > 0);
 		    int32_t cur_idx = old_idx;
 		    while(cur_idx != 0x0fffffff) {
-		    	HashNode& hnode = node->table[cur_idx];
+		    	HashNode& hnode = node->table[cur_idx - 1];
 		    	cur_idx = hnode.pointer;
-		    	if(!put_to_node(key, hnode.value, nodes_.size() - 1)) {
+		    	assert(values_.Get(hnode.value) != NULL);
+		    	if(!put_to_node(hnode.value, nodes_.size() - 1)) {
 		    		return Status::Corruption("mutation failed");
 		    	}
+		    }
+		    // now insert current record
+		    uint32_t new_record = 0x0fffffff;
+		    while(new_record == 0x0fffffff ) {
+  				new_record = values_.New();
+  			}
+  			char* p = values_.Get(new_record);
+  			memcpy(p, key.raw_ptr(), 8);
+  			value.write<0, 256>(p + 8);
+		    if(!put_to_node(new_record, nodes_.size() - 1)) {
+		    	return Status::Corruption("failed to insert after mutation");
 		    }
 		    int32_t new_idx = old_idx;
 		    while(!std::atomic_compare_exchange_strong(
 		    	&node->forward[key[level]],
 		    	&new_idx,
-		    	-nodes_.size()
+		    	-nodes_.size()+1
 		    	)) {
 		    	cur_idx = new_idx;
 		    	while(cur_idx != old_idx && cur_idx != 0x0fffffff) {
-		    		HashNode& hnode = node->table[cur_idx];
+		    		HashNode& hnode = node->table[cur_idx - 1];
 			    	cur_idx = hnode.pointer;
-			    	if(!put_to_node(key, hnode.value, nodes_.size() - 1)) {
-			    		return Status::Corruption("mutation failed");
+			    	if(!put_to_node(hnode.value, nodes_.size() - 1)) {
+			    		return Status::Corruption("incremental mutation failed");
 			    	}
 		    	}
 		    	old_idx = new_idx;
 		    }
-			  return Status::IOError("full hash table");
+		    return Status::OK();
  			}
  			level ++;
  		}
  		return Status::NotFound("missing key");
  	}
  	Status Delete(const Key& key) {
- 		return Status::OK();
+ 		HashTrieNode<hash_size_>::UnsafeRef node = nodes_[0].get();
+ 		unsigned char level = 0;
+ 		int32_t tmp;
+ 		// traverse by level
+ 		while(true) {
+ 			// find decend path
+ 			if( (tmp = -node->forward[key[level]]) > 0) {
+ 				// invalid path
+ 				if(tmp >= nodes_.size())
+ 					return Status::Corruption("access exceeds `HashTrieNode` vector");
+ 				node = nodes_[tmp].get();
+ 			} else if(tmp == 0) { // no prefix match
+ 				break;
+ 			} else { // find match
+		    // quadratic proding
+ 				unsigned int hash_val  = hash(key, level, 8) % hash_size_;
+		    unsigned int cur = hash_val;
+		    int offset = 1;
+		    do{
+		    	HashNode& hnode = node->table[cur];
+		    	if(hnode.pointer != 0 && check_delete(hnode.value, key, level)) {
+		    		return Status::OK();
+		    	}
+		      cur = (cur+ 2*offset - 1) % hash_size_;
+		      offset++;
+		    } while( offset < probe_depth_ && cur != hash_val);
+		    return Status::NotFound("missing key");
+ 			}
+ 			level ++;
+ 		}
+ 		return Status::NotFound("missing key");
  	}
  private:
  	static constexpr size_t hash_size_ = 512;
- 	static constexpr size_t probe_depth_ = 20;
+ 	static constexpr size_t probe_depth_ = 100;
  	std::vector<HashTrieNode<hash_size_>::Holder> nodes_;
  	PagedPool<8+256> values_;
  	uint32_t hash(const Key& key, int start, int end) {
@@ -214,6 +264,14 @@ class HashTrie {
  		uint32_t ret = 0;
  		while(start < end) {
  			ret = ret * seed + key[start++];
+ 		}
+ 		return ret;
+ 	}
+ 	uint32_t hash(const char* p, int start, int end) {
+ 		uint32_t seed = 131;
+ 		uint32_t ret = 0;
+ 		while(start < end) {
+ 			ret = ret * seed + p[start++];
  		}
  		return ret;
  	}
@@ -231,6 +289,16 @@ class HashTrie {
  		}
  		return false;
  	}
+ 	bool check_delete(uint32_t index, const Key& key, int level) {
+ 		if(index == 0x0fffffff) return false;
+ 		char* p = values_.Get(index);
+ 		if(p && key == p) {
+ 			*(reinterpret_cast<uint64_t*>(p)) = 0;
+ 			*(p+8) = key[level];
+ 			return true;
+ 		}
+ 		return false;
+ 	}
  	bool check_push(uint32_t index, const Key& key, const Value& value, int level) {
  		if(index == 0x0fffffff) return false;
  		char* p = values_.Get(index);
@@ -238,10 +306,8 @@ class HashTrie {
  			if(key == p) {
 	 			value.write<0, 256>(p + 8);
 	 			return true;
- 			} else if( *(reinterpret_cast<uint64_t*>(p)) == 0) {
- 				for(int i = 0; i < level + 1; i++) {
- 					if(*(p+8+i) != key[i]) return false; // different prefix
- 				}
+ 			} else if( *(reinterpret_cast<uint64_t*>(p)) == 0
+ 				&& *(p+8) == key[level]) {
  				memcpy(p, key.raw_ptr(), 8);
  				value.write<0,256>(p+8);
  				return true;
@@ -252,13 +318,13 @@ class HashTrie {
  	// exit on full
  	// no latch
  	// no overwrite
- 	bool put_to_node(const Key& key, uint32_t value_idx, uint32_t node_idx) {
+ 	bool put_to_node(uint32_t value_idx, uint32_t node_idx) {
  		HashTrieNode<hash_size_>::UnsafeRef node = nodes_[node_idx].get();
  		char* p = values_.Get(value_idx);
  		if(p && *(reinterpret_cast<uint64_t*>(p)) == 0) { return true; }
  		int level = node->level;
  		// quadratic proding
-		unsigned int hash_val  = hash(key, level, 8) % hash_size_;
+		unsigned int hash_val  = hash(p, level, 8) % hash_size_;
     int32_t cur = hash_val;
     int offset = 1;
     cur = hash_val;
@@ -270,14 +336,14 @@ class HashTrie {
     		// std::cout << "found null at " << cur << std::endl;
     		if(std::atomic_compare_exchange_strong(&hnode.pointer, 
     			&tmp, 
-    			node->forward[key[level]]
+    			node->forward[p[level]]
     			)) {
     			hnode.value = value_idx;
   				tmp = hnode.pointer;
     			while(!std::atomic_compare_exchange_strong(
-    				&node->forward[key[level]], 
+    				&node->forward[p[level]], 
     				&tmp,
-    				cur
+    				cur + 1
     				)) { hnode.pointer = tmp; }
   				return true;
     		}
--- test/hash_trie_test.cc
+++ test/hash_trie_test.cc
@@ -1,6 +1,6 @@
 #include <gtest/gtest.h>
 
-#define PORTAL_DEBUG
+// #define PORTAL_DEBUG
 #include "db/hash_trie.h"
 #include "portal-db/piece.h"
 
@@ -10,7 +10,7 @@ using namespace portal_db;
 
 TEST(HashTrieTest, BasicTest) {
 	HashTrie store("test_hash_trie");
-	size_t size = 100;
+	size_t size = 10000;
 	char buf[256];
 	for(int i = 0; i < size; i++) {
 		std::string tmp = std::to_string(i);
@@ -21,5 +21,24 @@ TEST(HashTrieTest, BasicTest) {
 		EXPECT_TRUE(store.Put(key, value).inspect());
 		EXPECT_TRUE(store.Get(key, value).inspect());
 		EXPECT_EQ(*(reinterpret_cast<const int*>(value.pointer_to_slice<0,4>())), i);
+	}
+}
+
+
+TEST(HashTrieTest, DeleteTest) {
+	HashTrie store("test_hash_trie");
+	size_t size = 1000;
+	char buf[256];
+	for(int i = 0; i < size; i++) {
+		std::string tmp = std::to_string(i);
+		tmp += std::string(8-tmp.size(), ' ');
+		*(reinterpret_cast<int*>(buf)) = i;
+		Key key(tmp.c_str());
+		Value value(buf);
+		EXPECT_TRUE(store.Put(key, value).inspect());
+		EXPECT_TRUE(store.Get(key, value).inspect());
+		EXPECT_EQ(*(reinterpret_cast<const int*>(value.pointer_to_slice<0,4>())), i);
+		EXPECT_TRUE(store.Delete(key).inspect());
+		EXPECT_TRUE(store.Get(key, value).IsNotFound());
 	}
 }
